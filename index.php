6814368d228157418eee80ca430f45cb1dea689b
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2025-09-24 23:23:31 +0200

    slider bijna af, alleen nog beter verplaatsen waar ik het wil hebben en kleine dingetjes aanpassen en dan voorledig klaar

--- index.php
+++ index.php
@@ -8,18 +8,20 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_forward" />
     <script src="script.js" defer></script>
-    <script src="swiper.js" defer></script>
-
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" /> -->
+    <script src="slide.js" defer></script>
+    <!-- swiper.js link -->
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
 </head>
 
 <body>
 
     <!-- Navigatiebar algemeen-->
-    <!-- logo links -->
     <div class="navbar">
         <div class="container">
             <div class="navbar-inside">
+                <!-- logo links -->
                 <div class="logo">
                     <img src="img/foto-me.jpg" alt="foto van wie ik ben">
                 </div>
@@ -51,90 +53,96 @@
             </div>
         </div>
     </div>
+    <main>
+        <!-- home page  -->
+        <section class="container">
+            <div class="info">
 
-    <!-- home page  -->
-    <section class="container">
-        <div class="info">
+                <div class="info-home">
+                    <h1>Hoi, ik ben Giorgina Cali</h1>
+                    <p>Ik ben 20 jaar en volg de opleiding Software Development aan het Media College Amsterdam,<br> waar ik nu in mijn derde jaar zit.<br>
+                        Mijn interesse en focus liggen vooral bij front-end en back-end development.</p>
+                    <button href="#" class="button-info">Klik hier voor meer informatie</button>
+                </div>
 
-            <div class="info-home">
-                <h1>Hoi, ik ben Giorgina Cali</h1>
-                <p>Ik ben 20 jaar en volg de opleiding Software Development aan het Media College Amsterdam,<br> waar ik nu in mijn derde jaar zit.<br>
-                    Mijn interesse en focus liggen vooral bij front-end en back-end development.</p>
-                <button href="#" class="button-info">Klik hier voor meer informatie</button>
+                <div class="info-foto-home">
+                    <img class="foto-me" src="img/foto-me.jpg" alt="foto van wie ik ben">
+                </div>
             </div>
+        </section>
 
-            <div class="info-foto-home">
-                <img class="foto-me" src="img/foto-me.jpg" alt="foto van wie ik ben">
+        <!-- Card slider -->
+        <section class="container">
+            <div class="card-wrapper">
+                <div class="swiper">
+                    <ul class="card-list swiper-wrapper">
+                        <!-- sv_unity -->
+                        <li class="card-item swiper-slide">
+                            <a href="#" class="card-link">
+                                <img src="img/SV-unity.png" alt="" class="card-image">
+                                <p class="badge">SV-Unity</p>
+                                <h2 class="card-title">De opdracht van dit project was om tijdens een gesprek met de klant hun ideeën te verzamelen en deze vervolgens zelfstandig uit te werken tot een passend eindproduct. </h2>
+                                <button class="card-button material-symbols-outlined">
+                                    arrow_forward
+                                </button>
+                            </a>
+                        </li>
+                        <!-- back end -->
+                        <li class="card-item swiper-slide">
+                            <a href="#" class="card-link">
+                                <img src="img/registratie.png" alt="" class="card-image">
+                                <p class="badge back_end">Back end</p>
+                                <h2 class="card-title">De opdracht van dit project was om de back-end security te versterken, zodat gebruikers niet te maken krijgen met broken access control.</h2>
+                                <button class="card-button material-symbols-outlined">
+                                    arrow_forward
+                                </button>
+                            </a>
+                        </li>
+                        <!-- nxt museum -->
+                        <li class="card-item swiper-slide">
+                            <a href="http://localhost/nxtmuseum/" class="card-link">
+                                <img src="img/nxtmuseum.png" alt="" class="card-image">
+                                <p class="badge museum">NXT museum</p>
+                                <h2 class="card-title">De opdracht van dit project was om een professionele, tweetalige museumwebsite te maken met informatie, afbeeldingen en bezoekersreviews.</h2>
+                                <button class="card-button material-symbols-outlined">
+                                    arrow_forward
+                                </button>
+                            </a>
+                        </li>
+                        <!-- webshop -->
+                        <li class="card-item swiper-slide">
+                            <a href="https://36725.hosts2.ma-cloud.nl/webshop_BO/" class="card-link">
+                                <img src="img/webshop.png" alt="" class="card-image">
+                                <p class="badge webshop">Webshop</p>
+                                <h2 class="card-title">De opdracht van dit project was om een webshop te maken op basis van de gegeven layout en stijl. Ik heb de webshop gebaseerd op Bol.com en voorzien van drie features die we moesten maken. </h2>
+                                <button class="card-button material-symbols-outlined">
+                                    arrow_forward
+                                </button>
+                            </a>
+                        </li>
+                        <!-- input output -->
+                        <li class="card-item swiper-slide">
+                            <a href="#" class="card-link">
+                                <img src="img/input_output.png" alt="" class="card-image">
+                                <p class="badge input_output">Input Output</p>
+                                <h2 class="card-title">De opdracht van dit project was om een input-output toepassing te maken. Mijn idee was een autootje te bouwen dat bestuurd wordt met een joystick, de joystick dient als input en het autootje als output. </h2>
+                                <button class="card-button material-symbols-outlined">
+                                    arrow_forward
+                                </button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </section>
-
-    <!-- Card slider -->
-    <section class="container">
-        <div class="card-wrapper">
-            <ul class="card-list">
-                <!-- sv_unity -->
-                <li class="card-item">
-                    <a href="#" class="card-link">
-                        <img src="img/SV-unity.png" alt="" class="card-image">
-                        <p class="badge">SV-Unity</p>
-                        <h2 class="card-title">De opdracht van dit project was om tijdens een gesprek met de klant hun ideeën te verzamelen en deze vervolgens zelfstandig uit te werken tot een passend eindproduct. </h2>
-                        <button class="card-button material-symbols-outlined">
-                            arrow_forward
-                        </button>
-                    </a>
-                </li>
-                <!-- back end -->
-                <li class="card-item">
-                    <a href="#" class="card-link">
-                        <img src="img/registratie.png" alt="" class="card-image">
-                        <p class="badge back_end">Back end</p>
-                        <h2 class="card-title">De opdracht van dit project was om de back-end security te versterken, zodat gebruikers niet te maken krijgen met broken access control.</h2>
-                        <button class="card-button material-symbols-outlined">
-                            arrow_forward
-                        </button>
-                    </a>
-                </li>
-                <!-- nxt museum -->
-                <li class="card-item">
-                    <a href="#" class="card-link">
-                        <img src="img/nxtmuseum.png" alt="" class="card-image">
-                        <p class="badge museum">NXT museum</p>
-                        <h2 class="card-title">De opdracht van dit project was om een professionele, tweetalige museumwebsite te maken met informatie, afbeeldingen en bezoekersreviews.</h2>
-                        <button class="card-button material-symbols-outlined">
-                            arrow_forward
-                        </button>
-                    </a>
-                </li>
-                <!-- webshop -->
-                <li class="card-item">
-                    <a href="#" class="card-link">
-                        <img src="img/webshop.png" alt="" class="card-image">
-                        <p class="badge webshop">Webshop</p>
-                        <h2 class="card-title">De opdracht van dit project was om een webshop te maken op basis van de gegeven layout en stijl. Ik heb de webshop gebaseerd op Bol.com en voorzien van drie features. </h2>
-                        <button class="card-button material-symbols-outlined">
-                            arrow_forward
-                        </button>
-                    </a>
-                </li>
-                <!-- input output -->
-                <li class="card-item">
-                    <a href="#" class="card-link">
-                        <img src="img/input_output.png" alt="" class="card-image">
-                        <p class="badge input_output">Input Output</p>
-                        <h2 class="card-title">De opdracht van dit project was om een input-output toepassing te maken. Mijn idee was een autootje te bouwen dat bestuurd wordt met een joystick, de joystick dient als input en het autootje als output. </h2>
-                        <button class="card-button material-symbols-outlined">
-                            arrow_forward
-                        </button>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </section>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-slide-button swiper-button-prev"></div>
+            <div class="swiper-slide-button swiper-button-next"></div>
+        </section>
 
 
-
-
+        <!-- linking swiperJS script -->
+        <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+    </main>
 </body>
 
 </html>
